9c649ee8f3e001c3abf86acfec98154389f62107
ustianschii <ustianschii@gmail.com>
2024-02-29 14:21:58 +0200

    finish to type, going to graph ql

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,18 +1,21 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import { selectCartItems } from '../../store/cart/cart.selector';
-import { addItemToCart } from '../../store/cart/cart.action';
-
-import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
-
+import { FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCartItems } from "../../store/cart/cart.selector";
+import { addItemToCart } from "../../store/cart/cart.action";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import {
   ProductCartContainer,
   Footer,
   Name,
   Price,
-} from './product-card.styles';
+} from "./product-card.styles";
+import { CategoryItem } from "../../store/categories/category.types";
 
-const ProductCard = ({ product }) => {
+type ProductCardProps = {
+  product: CategoryItem;
+};
+
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
   const { name, price, imageUrl } = product;
   const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
